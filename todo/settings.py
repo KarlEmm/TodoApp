e043294e42b5037d80308df4a5d63301ef596cd6
Karl <Karl.Bast@gmail.com>
2020-09-10 20:24:02 -0400

    First view in place.

--- todo/settings.py
+++ todo/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # My apps
+    'todos',
+
+    # Default django apps.
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -105,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'US/Eastern'
 
 USE_I18N = True
 
--- todos/models.py
+++ todos/models.py
@@ -2,10 +2,12 @@ from django.db import models
 
 class Task(models.Model):
     """A task the user has to do."""
-    text = models.CharField(max_length=200)
+    main = models.CharField(max_length=150, null=True)
+    details = models.TextField(null=True)
+    isdone = models.BooleanField(default=False)
     date_added = models.DateTimeField(auto_now_add=True)
     date_due = models.DateTimeField()
 
     def __str__(self):
         """Return a string representation of the model."""
-        return self.text
+        return self.main
